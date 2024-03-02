e558ce8215373b32db16855e396e070cfc252ce3
atuljoc <161138324+atuljoc@users.noreply.github.com>
2024-03-02 17:07:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let hrs=document.getElementById("hrs")
+let min=document.getElementById("min")
+let sec=document.getElementById("sec")
+setInterval(() => {
+    let currenttime=new Date();
+
+    console.log(currenttime)
+    hrs.innerHTML=currenttime.getHours();
+    min.innerHTML=currenttime.getMinutes();
+    sec.innerHTML=currenttime.getSeconds()  
+},1000);
